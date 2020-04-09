472ae54b59ef9e461c255c894dcc2adc432eb2f8
Jason <tzuchieh.hung@gmail.com>
2020-04-10 07:02:42 +0800

    Add User

---
+++ Library_5200/src/main/java/edu/northeastern/cs5200/models/User.java
@@ -0,0 +1,104 @@
+package edu.northeastern.cs5200.models;
+
+import java.sql.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer userId;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String email;
+	private Date dateOfBirth;
+
+	public User() {
+
+	}
+
+	public User(Integer id, String firstName, String lastName, String username, String password, String email,
+			Date dateOfBirth) {
+		this.userId = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Integer getId() {
+		return userId;
+	}
+
+	public void setId(Integer id) {
+		this.userId = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	@Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName=" + lastName + '\'' +
+                ", username=" + username + '\'' +
+                ", password=" + password + '\'' +
+                ", email='" + email +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
